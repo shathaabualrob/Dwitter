30d97146efa3010fc9d478c3fdb75609d4a3853c
Shatha AR <shatharub5@gmail.com>
2022-11-08 20:50:09 +0200

    Inherit models and implement signals

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User,Group
+from .models import Profile
+
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"] # Only display the "username" field
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Profile)
--- dwitter/models.py
+++ dwitter/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# dwitter/models.py
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
+    )
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+# Create a Profile for each new user.
+post_save.connect(create_profile, sender=User)
